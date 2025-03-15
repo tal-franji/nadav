a61c3f2f930872ddd90ab98002ffced1fe132765
Tal Franji <tal.franji@gmail.com>
2025-03-15 13:22:02 +0200

    misc

--- inheritance/game.py
+++ inheritance/game.py
@@ -52,20 +52,26 @@ class CastleCell:
 
 class Castle:
     def __init__(self):
-        self.cells: List[List[CastleCell]] = list()
-        # we build a matrix with 3 line in lenght 2,3,2
-        self.cells.append(list(CastleCell(), CastleCell()))  # first line
-        self.cells.append(list(CastleCell(), CastleCell(), CastleCell()))  # first line
-        self.cells.append(list(CastleCell(), CastleCell()))  # first line
-        def c(x,y):
-            return self.cells[y][x]
-        c(0, 0).neighbours = list(c(0,1), c(1,0), c(1,1))
-        c(0, 1).neighbours = list(c(0,0), c(1,1), c(1,2))
-        c(1, 0).neighbours = list(c(0,0), c(1,1), c(2,0))
-        c(1, 1).neighbours = list(c(0,0), c(0,1), c(1,0), c(1,2), c(2,0), c(2,1))
-        c(1, 2).neighbours = list(c(0,1), c(1,1), c(2,1))
-        c(2, 0).neighbours = list(c(1,0), c(1,1), c(2,1))
-        c(2, 1).neighbours = list(c(2,0), c(1,1), c(1,2))
+        self.cells: List[List[CastleCell]] = [[None] * 2, [None] * 3, [None] * 2]
+        # we build a matrix with 3 line in length 2,3,2
+        self.cells.append([CastleCell(), CastleCell()])  # first line
+        self.cells.append([CastleCell(), CastleCell(), CastleCell()])  # first line
+        self.cells.append([CastleCell(), CastleCell()])  # first line
+
+        def c(x, y):
+            cell = self.cells[x][y]
+            if cell is None:
+                cell = CastleCell()
+                self.cells[x][y] = cell
+            return cell
+
+        c(0, 0).neighbours = [c(0, 1), c(1, 0), c(1, 1)]
+        c(0, 1).neighbours = [c(0, 0), c(1, 1), c(1, 2)]
+        c(1, 0).neighbours = [c(0, 0), c(1, 1), c(2, 0)]
+        c(1, 1).neighbours = [c(0, 0), c(0, 1), c(1, 0), c(1, 2), c(2, 0), c(2, 1)]
+        c(1, 2).neighbours = [c(0, 1), c(1, 1), c(2, 1)]
+        c(2, 0).neighbours = [c(1, 0), c(1, 1), c(2, 1)]
+        c(2, 1).neighbours = [c(2, 0), c(1, 1), c(1, 2)]
     
 
 class Player:
@@ -76,19 +82,36 @@ class Player:
         self.active = True
     
     def play(self, round: str, table: "Table"):
+        if round == "build":
+            self.build(table)
+        elif round == "activate":
+            self.activate(table)
+        else:
+            raise ValueError(f"bad round {round}")
+
+    def build(self, table: "Table"):
         pass
 
+    def activate(self, table: "Table"):
+        pass
+
+
+class PlayerRandom(Player):
+    pass
+
+
 class Table:
-    def __init__(self, names: List[str]):
-        self.players: List[Player] = [Player(name) for name in names]
+    def __init__(self, names: List[str], player_class=Player):
+        self.players: List[Player] = [player_class(name) for name in names]
 
     def shift(self):
         player0 = self.players.pop(0)
         self.players.append(player0)
 
+
 class Game:
-    def __init__(self, names: List[str]):
-        self.table = Table(names)
+    def __init__(self, names: List[str], player_class=Player):
+        self.table = Table(names, player_class)
         self.rounds: List[str] = ["build", "activate"]
         self.cur_round_index = 0
         self.cycle_count = 0
@@ -99,9 +122,15 @@ class Game:
         for self.cur_round_index, round in enumerate(self.rounds):
             for self.cur_player_index, player in enumerate(self.table.players):
                 player.play(round, self.table)
+        self.table.shift()
 
 
+def main():
+    game = Game(["Alice", "Bob", "Charlie"], PlayerRandom)
+    game.run_game_cycle()
 
-print("DEBUG>>>>") 
-r = Reserve()
+
+if __name__ == "__main__":
+    main()
+
     
